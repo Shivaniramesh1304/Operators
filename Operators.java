1685e0f2e72f3ac4c707e66208f5d4758443580c
Shivaniramesh1304 <shivaniramesh1304@gmail.com>
2025-01-30 21:47:47 +0530

    Add files via upload

---
+++ Operators.java
@@ -0,0 +1,16 @@
+public class Operators {
+    public static void main(String[ ]args){
+        int a=25;
+        int b=15;
+        int sum=a+b;
+        System.out.println("sum:"+sum);
+        int differences=a-b;
+        System.out.println("difference:"+differences);
+        int product=a*b;
+        System.out.println("product:"+product);
+        int division=a/b;
+        System.out.print("division:"+division);
+        int remainder=a%b;
+        System.out.println("remainder:"+remainder);
+    }
+}
